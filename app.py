85d47f0fdfa2ad3fada40bb86b3357b1b898a3ba
Tharindu Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2024-09-28 11:36:10 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import pickle
+import pandas as pd
+
+# Load the trained model
+with open('ModelName.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    df = pd.DataFrame(data)
+    prediction = model.predict(df)
+    return jsonify({'prediction': prediction.tolist()})
+
+if __name__ == '__main__':
+    app.run(debug=True)
