f1761d60f8cdb67aab97aafed8dcfb0e273b7cf8
Tharindu Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2024-09-28 23:30:30 +0530

    updated app.py

--- app.py
+++ app.py
@@ -8,12 +8,22 @@ with open('./model/best_model.pkl', 'rb') as file:
 
 app = Flask(__name__)
 
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     data = request.json
+#     df = pd.DataFrame(data)
+#     prediction = model.predict(df)
+#     return jsonify({'prediction': prediction.tolist()})
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
-    df = pd.DataFrame(data)
-    prediction = model.predict(df)
-    return jsonify({'prediction': prediction.tolist()})
+    data = request.get_json()
+    # Perform prediction logic here
+    prediction = [0]  # Example prediction
+    return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
     app.run(debug=True)
