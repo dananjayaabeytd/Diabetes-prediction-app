fbe4b0b60048fcf842bda32fcc7f4d48ef14bd55
Tharindu Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2024-10-02 13:02:35 +0530

    url update again

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import pickle
+from flask_cors import CORS
 import pandas as pd
 
 # Load the trained model
@@ -7,13 +8,14 @@ with open('final_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)  # Temporarily allow all origins for testing
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.data
-    df = pickle.loads(data)
+    data = request.json
+    df = pd.DataFrame(data)
     prediction = model.predict(df)
-    return pickle.dumps({'prediction': prediction.tolist()})
+    return jsonify({'prediction': prediction.tolist()})
 
 if __name__ == '__main__':
     app.run(debug=True)
